0f5c02b732d0d5bbe389cf0c8f9c1361026900ef
Matt Bertolini <viper2843@gmail.com>
2011-05-22 11:58:20 -0400

    Updating init script to work with either wget or curl depending on what it finds.

--- inithome.sh
+++ inithome.sh
@@ -4,9 +4,13 @@
 # Written by Matt Bertolini
 
 # Check to see if the tools required for this script to run exist
+type -P curl &>/dev/null
+HAS_CURL=$?
 type -P wget &>/dev/null
-if [ $? -ne 0 ]; then
-	echo "Cannot proceed. No wget executable found."
+HAS_WGET=$?
+
+if [[ $HAS_CURL -ne 0 && $HAS_WGET -ne 0 ]]; then
+	echo "Cannot proceed. No curl or wget executable found."
 	exit 1
 fi
 
@@ -15,6 +19,25 @@ if [ $? -ne 0 ]; then
 	echo "Cannot proceed. No uname executable found."
 	exit 1
 fi
+
+function downloadWithCurl() {
+	curl $1 -o $2
+}
+
+function downloadWithWget() {
+	wget --no-check-certificate $1 -O $2
+}
+
+function download() {
+	if [ $HAS_CURL -eq 0 ]; then
+		downloadWithCurl $1 $2
+	elif [ $HAS_WGET -eq 0 ]; then
+		downloadWithWget $1 $2
+	else
+		echo "Cannot proceed. No curl or wget executable found."
+		exit 1
+	fi
+}
 
 # Make sure we are in the user's home directory
 cd ~
@@ -36,16 +59,16 @@ OPSYS=$(uname -o 2>/dev/null || uname -s)
 # Download files common to all platforms. Please note that I am not checking 
 # the SSL certificate because of the way Github and wget handle wildcard certificates.
 echo "Downloading universal profile files..."
-wget --no-check-certificate https://github.com/MaliciousMonkey/profilesettings/raw/master/profile -O .profile
-wget --no-check-certificate https://github.com/MaliciousMonkey/profilesettings/raw/master/bashrc -O .bashrc
-wget --no-check-certificate https://github.com/MaliciousMonkey/profilesettings/raw/master/bash_aliases -O .bash_aliases
-wget --no-check-certificate https://github.com/MaliciousMonkey/profilesettings/raw/master/vimrc -O .vimrc
+download https://github.com/MaliciousMonkey/profilesettings/raw/master/profile .profile
+download https://github.com/MaliciousMonkey/profilesettings/raw/master/bashrc .bashrc
+download https://github.com/MaliciousMonkey/profilesettings/raw/master/bash_aliases .bash_aliases
+download https://github.com/MaliciousMonkey/profilesettings/raw/master/vimrc .vimrc
 
 #Download platform specific files
 
 if [ $OPSYS == "Cygwin" ]; then
 	echo "Downloading Cygwin specific profile files..."
-	wget --no-check-certificate https://github.com/MaliciousMonkey/profilesettings/raw/master/minttyrc -O .minttyrc
+	download https://github.com/MaliciousMonkey/profilesettings/raw/master/minttyrc .minttyrc
 fi
 
 # Finally, source the profile files
