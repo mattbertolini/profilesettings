878046d1a2cb8f0f9f42fc390c4597e7c65b884c
mattbertolini <viper2843@gmail.com>
2025-02-01 10:13:04 -0500

    Start setting up bash. Other tweaks.

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,10 @@
 set fish_greeting
 
+set -x EDITOR "vim"
+set -x LESS "-M -I -R"
+
+alias finder="open -a finder"
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
