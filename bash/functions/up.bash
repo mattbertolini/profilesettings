6dc26892906f1542c16fa988906e1886b038bdd2
mattbertolini <viper2843@gmail.com>
2025-02-01 15:01:56 -0500

    Fix warnings

--- bash/functions/up.bash
+++ bash/functions/up.bash
@@ -8,9 +8,9 @@ function up() {
 	if [ -n "$1" ]; then
 		COUNT=$1
 	fi
-	for ((i = 1; i <= $COUNT; i++)); do
+	for ((i = 1; i <= COUNT; i++)); do
 		# Making a string so we change the directory all in one shot.
 		CD_STR=$CD_STR"../"
 	done
-	cd $CD_STR
+	cd $CD_STR || exit
 }
