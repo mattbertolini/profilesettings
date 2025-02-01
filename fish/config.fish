466da39a759ba9164be55dd96ca7df86d47aebfa
mattbertolini <viper2843@gmail.com>
2025-02-01 14:16:35 -0500

    Add user path

--- fish/config.fish
+++ fish/config.fish
@@ -6,9 +6,14 @@ set fish_greeting
 set -x EDITOR "vim"
 set -x LESS "-M -I -R"
 # Highlight grep matches
-set -x GREP_OPTIONS="--color=auto"
+set -x GREP_OPTIONS "--color=auto"
 
 alias finder="open -a finder"
+
+# Set PATH so it includes user's private bin if it exists
+if test -d ~/bin
+    fish_add_path -p ~/bin
+end
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
