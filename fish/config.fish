951b48b551692cbbf2530fcab81a7e37f1d64892
mattbertolini <viper2843@gmail.com>
2025-02-01 10:56:51 -0500

    Add ql function

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,12 @@
+# Fish Shell Profile
+
 set fish_greeting
 
+# Set up environment variables
 set -x EDITOR "vim"
 set -x LESS "-M -I -R"
+# Highlight grep matches
+set -x GREP_OPTIONS="--color=auto"
 
 alias finder="open -a finder"
 
---
+++ fish/functions/ql.fish
@@ -0,0 +1,3 @@
+function ql --description "Run MacOS quicklook"
+    qlmanage -p $argv > /dev/null 2>&1; echo
+end
