38d75e8ddc62622578c2adda2a7d6020b73470b6
mattbertolini <viper2843@gmail.com>
2025-02-01 13:56:42 -0500

    Use environment varaible for homebrew

--- bash/conf.d/lesspipe.bash
+++ bash/conf.d/lesspipe.bash
@@ -1,4 +1,4 @@
-# make less more friendly for non-text input files, see lesspipe(1)
-if [ -x "$(brew --prefix)/bin/lesspipe.sh" ]; then
-    export LESSOPEN="|$(brew --prefix)/bin/lesspipe.sh %s"
+# Make less more friendly for non-text input files, see lesspipe(1)
+if [ -x "$HOMEBREW_PREFIX/bin/lesspipe.sh" ]; then
+    export LESSOPEN="|$HOMEBREW_PREFIX/bin/lesspipe.sh %s"
 fi
