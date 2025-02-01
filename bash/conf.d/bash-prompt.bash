c512e48368b95b53020da86640e891b72de9f518
mattbertolini <viper2843@gmail.com>
2025-02-01 14:30:31 -0500

    Update comment

--- bash/conf.d/bash-prompt.bash
+++ bash/conf.d/bash-prompt.bash
@@ -3,7 +3,7 @@ case "$TERM" in
 xterm-color|xterm-256color|screen-color|screen-256color|xterm-ghostty)
     # Default prompt. Light blue and light green
     PS1='\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
-    # Alternate light red and yellow prompt. Copy to bash_local if you want to use.
+    # Alternate light red and yellow prompt. Uncomment if you want to use.
     #PS1='\[\033[01;31m\]\u@\h\[\033[00m\]:\[\033[01;33m\]\w\[\033[00m\]\$ '
     ;;
 *)
