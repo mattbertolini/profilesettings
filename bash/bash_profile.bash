573b84fb8e16bd633ae775a1245abca224f5c83d
mattbertolini <viper2843@gmail.com>
2025-02-02 11:36:50 -0500

    Rename files to make them easier to work with.

--- bash/bash_profile.bash
+++ bash/bash_profile.bash
@@ -1,7 +1,7 @@
 # Bash Shell Profile
 
 # Set up environment variables
-EDITOR=vim
+EDITOR=vim; export EDITOR
 LESS="-M -I -R"; export LESS
 # Highlight grep matches
 GREP_OPTIONS='--color=auto'; export GREP_OPTIONS
@@ -18,16 +18,18 @@ if [ -d ~/bin ]; then
     PATH=~/bin:"${PATH}"
 fi
 
-BASH_PROFILE_DIR="$(cd "$(dirname $(realpath "${BASH_SOURCE[0]}"))" && pwd)"
+BASH_PROFILE_DIR="$(cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")" && pwd)"
 
 # Source files in conf.d directory
 for file in "${BASH_PROFILE_DIR}/conf.d/"*.bash; do
     [[ -f "${file}" ]] || continue
+    # shellcheck source=conf.d/bash-prompt.bash
     source "${file}"
 done
 
 # Source files in functions directory
 for file in "${BASH_PROFILE_DIR}/functions/"*.bash; do
     [[ -f "${file}" ]] || continue
+    # shellcheck source=functions/up.bash
     source "${file}"
 done
