d31f3e784f14d98977022ba71309a946e02b0d8e
mattbertolini <viper2843@gmail.com>
2025-02-02 15:19:24 -0500

    Disable homebrew right now.

--- install.sh
+++ install.sh
@@ -33,11 +33,11 @@ OS=$(uname -o 2>/dev/null || uname -s)
 if [ "$OS" = "Darwin" ]; then
     echo "Detected MacOS"
 
-    if [ ! "$(which brew)" ]; then
-        echo "Installing homebrew"
-        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-        brew bundle install
-    fi
+#    if [ ! "$(which brew)" ]; then
+#        echo "Installing homebrew"
+#        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+#        brew bundle install
+#    fi
 fi
 
 ./bash/install.sh
