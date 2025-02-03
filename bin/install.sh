4e2235d30599bc4c258e256632288712d9ea19cf
mattbertolini <viper2843@gmail.com>
2025-02-03 08:03:19 -0500

    Fix install script

--- bin/install.sh
+++ bin/install.sh
@@ -6,11 +6,11 @@ REPO_ROOT="$(cd "$(dirname "$0")" && pwd -P)/.."
 
 . "${REPO_ROOT}/scripts/core.sh"
 
-NAMESPACE="mrb"
+OVERWRITE="${overwrite:-false}"
 HOME_BIN_DIR="${HOME}/bin"
 
 echo "Installing local bin"
 
 # Custom scripts
 mkdir -p "${HOME_BIN_DIR}"
-copyFile "${REPO_ROOT}/bin/findclass" "${HOME_BIN_DIR}/findclass"
+copyFile "${REPO_ROOT}/bin/findclass" "${HOME_BIN_DIR}/findclass" "${OVERWRITE}"
