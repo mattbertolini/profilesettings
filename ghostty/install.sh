1ba6a07942253042346631aecb60d9af10db791c
mattbertolini <viper2843@gmail.com>
2025-02-03 07:41:54 -0500

    Add overwrite support

--- ghostty/install.sh
+++ ghostty/install.sh
@@ -6,10 +6,11 @@ REPO_ROOT="$(cd "$(dirname "$0")" && pwd -P)/.."
 
 . "${REPO_ROOT}/scripts/core.sh"
 
+OVERWRITE="${overwrite:-false}"
 GHOSTTY_CONFIG_DIR="${HOME}/.config/ghostty"
 
 echo "Installing Ghostty config"
 
 # Main config
 mkdir -p "${GHOSTTY_CONFIG_DIR}"
-copyFile "${REPO_ROOT}/ghostty/config" "${GHOSTTY_CONFIG_DIR}/config"
+copyFile "${REPO_ROOT}/ghostty/config" "${GHOSTTY_CONFIG_DIR}/config" "${OVERWRITE}"
--- vim/install.sh
+++ vim/install.sh
@@ -6,13 +6,14 @@ REPO_ROOT="$(cd "$(dirname "$0")" && pwd -P)/.."
 
 . "${REPO_ROOT}/scripts/core.sh"
 
-NAMESPACE="mrb"
+OVERWRITE="${overwrite:-false}"
+NAMESPACE="${namespace:-mrb}"
 VIM_CONFIG_DIR="${HOME}/.config/${NAMESPACE}/vim"
 
 echo "Installing Vim config"
 
 # Main config
 mkdir -p "${VIM_CONFIG_DIR}"
-copyFile "${REPO_ROOT}/vim/vimrc" "${VIM_CONFIG_DIR}/vimrc"
+copyFile "${REPO_ROOT}/vim/vimrc" "${VIM_CONFIG_DIR}/vimrc" "${OVERWRITE}"
 
 createSymlink "${VIM_CONFIG_DIR}/vimrc" "${HOME}/.vimrc"
