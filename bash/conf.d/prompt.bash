788e37ba1048fe501b75193a4d93193f0111b4ee
mattbertolini <viper2843@gmail.com>
2025-02-10 17:49:03 -0500

    Shrink the path in the bash prompt

--- bash/conf.d/prompt.bash
+++ bash/conf.d/prompt.bash
@@ -10,21 +10,32 @@ __setPs1() {
         exit_code_prompt=" ${bold}${red}[$exit_code]${reset}"
     fi
 
+    # Use a shrunken path format (e.g. ~/o/p/test for ~/opt/projects/test)
+    local cur_dir="${PWD/#$HOME/\~}"
+    local shrunk_cur_dir="$(__shrink_path "${cur_dir}")"
+
     case "$TERM" in
     xterm-color|xterm-256color|screen-color|screen-256color|xterm-ghostty)
         # Default prompt. Light blue and light green
-        PS1="\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]$exit_code_prompt\$ "
+        #PS1="\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]$exit_code_prompt\$ "
+        PS1="\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]$shrunk_cur_dir\[\033[00m\]$exit_code_prompt\$ "
         # Alternate light red and yellow prompt. Uncomment if you want to use.
         #PS1='\[\033[01;31m\]\u@\h\[\033[00m\]:\[\033[01;33m\]\w\[\033[00m\]\$ '
         ;;
     *)
-        PS1='\u@\h:\w\$ '
+        PS1="\u@\h:$shrunk_cur_dir\$ "
+#        PS1='\u@\h:\w\$ '
         ;;
     esac
 }
 
 __setTitle() {
-    echo -ne "\033]0;${USER}@${HOSTNAME}: ${PWD/$HOME/~}\007"
+    local cur_dir="${PWD/#$HOME/\~}"
+    local shrunk_cur_dir="$(__shrink_path "${cur_dir}")"
+
+    local host="$(hostname -s)"
+
+    echo -ne "\033]0;${USER}@${host}: ${shrunk_cur_dir}\007"
 }
 
 # If this is an xterm set the title to user@host:dir
