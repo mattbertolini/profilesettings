9553ea9ffb1a9cc22c822317eadef0be86239ef6
mattbertolini <viper2843@gmail.com>
2025-02-10 18:03:36 -0500

    Add git branch if present

--- bash/conf.d/prompt.bash
+++ bash/conf.d/prompt.bash
@@ -10,6 +10,12 @@ __setPs1() {
         exit_code_prompt=" ${bold}${red}[$exit_code]${reset}"
     fi
 
+    # Add git branch if present
+    local git_prompt=''
+    if [ "$(which git)" ]; then
+        git_prompt="$(__git_ps1)"
+    fi
+
     # Use a shrunken path format (e.g. ~/o/p/test for ~/opt/projects/test)
     local cur_dir="${PWD/#$HOME/\~}"
     local shrunk_cur_dir="$(__shrink_path "${cur_dir}")"
@@ -18,7 +24,7 @@ __setPs1() {
     xterm-color|xterm-256color|screen-color|screen-256color|xterm-ghostty)
         # Default prompt. Light blue and light green
         #PS1="\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]$exit_code_prompt\$ "
-        PS1="\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]$shrunk_cur_dir\[\033[00m\]$exit_code_prompt\$ "
+        PS1="\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]$shrunk_cur_dir\[\033[00m\]$git_prompt$exit_code_prompt\$ "
         # Alternate light red and yellow prompt. Uncomment if you want to use.
         #PS1='\[\033[01;31m\]\u@\h\[\033[00m\]:\[\033[01;33m\]\w\[\033[00m\]\$ '
         ;;
