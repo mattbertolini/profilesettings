7a7164b4ebf08e02935e9e3ed24ff616a2406f6f
Matt Bertolini <mattbertolini@users.noreply.github.com>
2025-03-11 21:53:53 -0400

    Add editorconfig support in vim

--- vim/install.sh
+++ vim/install.sh
@@ -9,11 +9,16 @@ REPO_ROOT="$(cd "$(dirname "$0")" && pwd -P)/.."
 OVERWRITE="${overwrite:-false}"
 NAMESPACE="${namespace:-mrb}"
 VIM_CONFIG_DIR="${HOME}/.config/${NAMESPACE}/vim"
+VIM_CONF_D_DIR="${VIM_CONFIG_DIR}/conf.d"
 
 echo "Installing Vim config"
 
 # Main config
 mkdir -p "${VIM_CONFIG_DIR}"
 copyFile "${REPO_ROOT}/vim/vimrc.vim" "${VIM_CONFIG_DIR}/vimrc.vim" "${OVERWRITE}"
+
+# conf.d directory
+mkdir -p "${VIM_CONF_D_DIR}"
+copyFile "${REPO_ROOT}/vim/conf.d/editorconfig.vim" "${VIM_CONF_D_DIR}/editorconfig.vim" "${OVERWRITE}"
 
 createSymlink "${VIM_CONFIG_DIR}/vimrc.vim" "${HOME}/.vimrc"
